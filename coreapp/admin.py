b46267ab292f37edb4c14b3ada0408b96f6a8007
mendibaev_i <mendibaev_i@ufanet.ru>
2022-05-10 15:57:32 +0500

    init admin

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from coreapp.models import Product, Article, Brand
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["article", "name"]
+    search_fields = ["article", "name"]
+    list_filter = ["article__brand"]
+    raw_id_fields = ['article', ]
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["art", "brand"]
+    search_fields = ["art", "brand"]
+    list_filter = ["brand"]
+    raw_id_fields = ['brand']
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
