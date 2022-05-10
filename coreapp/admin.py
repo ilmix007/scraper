299611b8c08b443a0c6d9769dec6bf3b05ed58a9
mendibaev_i <mendibaev_i@ufanet.ru>
2022-05-10 16:39:39 +0500

    init admin and models

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,25 +1,38 @@
 from django.contrib import admin
 
-from coreapp.models import Product, Article, Brand
+from coreapp.models import Product, Article, Brand, Offer, Shop
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["article", "name"]
-    search_fields = ["article", "name"]
-    list_filter = ["article__brand"]
+    list_display = ['article', 'name']
+    search_fields = ['article', 'name']
+    list_filter = ['brand']
     raw_id_fields = ['article', ]
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ["art", "brand"]
-    search_fields = ["art", "brand"]
-    list_filter = ["brand"]
-    raw_id_fields = ['brand']
+    list_display = ['art']
+    search_fields = ['art']
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-    search_fields = ["name"]
+    list_display = ['name']
+    search_fields = ['name']
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ['product', 'shop', 'count']
+    search_fields = ['product', 'shop']
+    list_filter = ['product__brand']
+    raw_id_fields = ['product', 'shop']
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'phone']
+    search_fields = ['name', 'address', 'phone']
+    raw_id_fields = ['site']
--- coreapp/models/products.py
+++ coreapp/models/products.py
@@ -6,10 +6,7 @@ from coreapp.models.sites import Shop
 
 class Brand(CreatedMixin, UpdatedMixin, models.Model):
     """ Бренды """
-    name = models.CharField(verbose_name='Наименование', max_length=67, unique=True)
-
-    def __str__(self):
-        return str(self.name)
+    name = models.CharField(verbose_name='Наименование', max_length=63, unique=True)
 
     class Meta:
         verbose_name = 'Бренд'
@@ -18,34 +15,36 @@ class Brand(CreatedMixin, UpdatedMixin, models.Model):
 
 class Article(CreatedMixin, UpdatedMixin, models.Model):
     """ Артикул """
-    art = models.CharField(verbose_name='Артикул', max_length=255)
-    brand = models.ForeignKey(Brand, verbose_name='Бренд', related_name='articles', on_delete=models.PROTECT)
+    art = models.CharField(verbose_name='Артикул', max_length=255, unique=True)
 
     class Meta:
         verbose_name = 'Артикул'
         verbose_name_plural = 'Артикулы'
 
 
+class ImgLink(CreatedMixin, UpdatedMixin, models.Model):
+    url = models.URLField(verbose_name='Ссылка', max_length=255, unique=True)
+    alt = models.CharField(verbose_name='Альтернативный текст', max_length=63)
+
+
 class Product(CreatedMixin, UpdatedMixin, models.Model):
     """ Товар """
+    brand = models.ForeignKey(Brand, verbose_name='Бренд', related_name='articles', on_delete=models.PROTECT)
     article = models.ForeignKey(Article, verbose_name='Артикул', related_name='products',
                                 on_delete=models.PROTECT, null=True, blank=True)
     name = models.CharField(verbose_name='Наименование', max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
+    img = models.ManyToManyField(ImgLink, verbose_name='Изображения')
 
     class Meta:
         ordering = ['name']
-        unique_together = ("article", "name")
+        unique_together = ('article', 'name')
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
 
 
 class Parameter(CreatedMixin, UpdatedMixin, models.Model):
-    """ Параметры товаров """
-    # warehouse = models.ForeignKey(Warehouse, verbose_name='Склад', null=True, blank=True, on_delete=models.CASCADE)
-    # article = models.ForeignKey(Article, verbose_name='Артикул', null=True, blank=True, on_delete=models.CASCADE)
+    """ Параметр товара """
+    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
     name = models.CharField(verbose_name='Наименование', max_length=63)
     value = models.CharField(verbose_name='Значение', max_length=255)
 
@@ -57,20 +56,15 @@ class Parameter(CreatedMixin, UpdatedMixin, models.Model):
         verbose_name_plural = 'Параметры товаров'
 
 
-class ImgLink(CreatedMixin, UpdatedMixin, models.Model):
-    url = models.URLField(verbose_name='Ссылка', max_length=255, unique=True)
-    alt = models.CharField(verbose_name='Альтернативный текст', max_length=63)
-
-
 class Offer(CreatedMixin, UpdatedMixin, models.Model):
     """ Параметры товаров """
     product = models.ForeignKey(Product, verbose_name='Товар', related_name='offers', on_delete=models.CASCADE)
-    shop = models.ForeignKey(Shop, verbose_name='Магазин', related_name='offers', on_delete=models.PROTECT)
+    shop = models.ForeignKey(Shop, verbose_name='Магазин', related_name='offers', on_delete=models.CASCADE)
     link = models.URLField(verbose_name='Ссылка', max_length=255, unique=True)
     img = models.ManyToManyField(ImgLink, verbose_name='Изображения')
     count = models.IntegerField(verbose_name='Количество')
 
     class Meta:
-        unique_together = ("product", "shop")
+        unique_together = ('product', 'shop')
         verbose_name = 'Предложение'
         verbose_name_plural = 'Предложения'
