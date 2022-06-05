e64835654281d0456583c4a3a747a877822720f9
mendibaev_i <mendibaev_i@ufanet.ru>
2022-06-05 18:39:06 +0500

    init SiteFacade

---
+++ coreapp/service/sites.py
@@ -0,0 +1,23 @@
+from coreapp.models import Site, ParameterKey, SiteParameter
+
+
+class SiteFacade:
+    """Фасад для работы с сайтом"""
+
+    def __init__(self, site: Site):
+        self.site = site
+
+    def set_params(self, key, values: list):
+        key, _ = ParameterKey.objects.get_or_create(title=key, defaults={'title': key})
+        key.save()
+        if key.title == 'Clean-param':
+            for value in values:
+                vals = value.split('&')
+                for val in vals:
+                    param, _ = SiteParameter.objects.get_or_create(key=key, value=val, site=self.site,
+                                                                   defaults={'key': key, 'value': val,
+                                                                             'site': self.site})
+        else:
+            for val in values:
+                param, _ = SiteParameter.objects.get_or_create(key=key, value=val, site=self.site,
+                                                               defaults={'key': key, 'value': val, 'site': self.site})
