92c7b535e5881f6d48b6f813583c92818abb5bc4
mendibaev_i <mendibaev_i@ufanet.ru>
2022-06-05 15:20:14 +0500

    update mixins

--- coreapp/mixins/db.py
+++ coreapp/mixins/db.py
@@ -1,17 +1,33 @@
 from django.db import models
 
 
-class CreatedMixin(models.Model):
+class BaseMixin(models.Model):
+    """ Базовый класс миксинов """
+
+    class Meta:
+        abstract = True
+
+
+class CreatedMixin(BaseMixin):
     """ Добавляет поле created """
     created = models.DateTimeField(verbose_name='Добавлен', auto_now_add=True)
 
-    class Meta:
-        abstract = True
 
-
-class UpdatedMixin(models.Model):
+class UpdatedMixin(BaseMixin):
     """ Добавляет поле updated """
     updated = models.DateTimeField(verbose_name='Изменен', auto_now=True)
 
-    class Meta:
-        abstract = True
+
+class StartProcessMixin(BaseMixin):
+    """ Добавляет поле last_process """
+    start_process = models.DateTimeField(verbose_name='Начало обработки')
+
+
+class FinishProcessMixin(BaseMixin):
+    """ Добавляет поле finish_process """
+    finish_process = models.DateTimeField(verbose_name='Конец обработки')
+
+
+class LastProcessMixin(BaseMixin):
+    """ Добавляет поле last_process """
+    last_process = models.DateTimeField(verbose_name='Последняя обработка')
