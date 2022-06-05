8f0310da65a9d01465238741c7c100ed35e9451d
mendibaev_i <mendibaev_i@ufanet.ru>
2022-06-05 15:28:39 +0500

    update mixins

--- coreapp/mixins/db.py
+++ coreapp/mixins/db.py
@@ -12,22 +12,37 @@ class CreatedMixin(BaseMixin):
     """ Добавляет поле created """
     created = models.DateTimeField(verbose_name='Добавлен', auto_now_add=True)
 
+    class Meta:
+        abstract = True
+
 
 class UpdatedMixin(BaseMixin):
     """ Добавляет поле updated """
     updated = models.DateTimeField(verbose_name='Изменен', auto_now=True)
 
+    class Meta:
+        abstract = True
+
 
 class StartProcessMixin(BaseMixin):
-    """ Добавляет поле last_process """
-    start_process = models.DateTimeField(verbose_name='Начало обработки')
+    """ Добавляет поле start_process """
+    start_process = models.DateTimeField(verbose_name='Начало обработки', null=True, blank=True)
+
+    class Meta:
+        abstract = True
 
 
 class FinishProcessMixin(BaseMixin):
     """ Добавляет поле finish_process """
-    finish_process = models.DateTimeField(verbose_name='Конец обработки')
+    finish_process = models.DateTimeField(verbose_name='Конец обработки', null=True, blank=True)
+
+    class Meta:
+        abstract = True
 
 
 class LastProcessMixin(BaseMixin):
     """ Добавляет поле last_process """
-    last_process = models.DateTimeField(verbose_name='Последняя обработка')
+    last_process = models.DateTimeField(verbose_name='Последняя обработка', null=True, blank=True)
+
+    class Meta:
+        abstract = True
