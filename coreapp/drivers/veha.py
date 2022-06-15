10845b98db206441568db47482771c00b62c4742
mendibaev_i <mendibaev_i@ufanet.ru>
2022-06-16 00:15:06 +0500

    update Veha.parse_link_type

--- coreapp/drivers/veha.py
+++ coreapp/drivers/veha.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-
+import re
 from coreapp.drivers.base import BaseDriver, Link
 
 import logging
@@ -10,5 +10,7 @@ LOGGER = logging.getLogger(__name__)
 class Veha(BaseDriver):
 
     def parse_link_type(self, soup: BeautifulSoup, link: Link) -> Link:
-        """Обновляет тип ссылки"""
+        """Определяет тип ссылки"""
+        if len(re.findall(r'/catalog/product/\d*', link.url)) > 0:
+            link.type_link.product = True
         return link
