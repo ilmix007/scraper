6af9718e45a451129c336b74945eb0138d7bed94
mendibaev_i <mendibaev_i@ufanet.ru>
2022-06-16 16:41:17 +0500

    init Veha.get_shops

--- coreapp/drivers/veha.py
+++ coreapp/drivers/veha.py
@@ -2,7 +2,7 @@ from typing import List
 
 from bs4 import BeautifulSoup
 import re
-from coreapp.drivers.base import BaseDriver, Link, Offer, Product, Parameter
+from coreapp.drivers.base import BaseDriver, Link, Offer, Product, Parameter, Shop
 
 import logging
 
@@ -23,7 +23,6 @@ class Veha(BaseDriver):
             link.type_link.product = True
         return link
 
-
     def get_price(self, soup: BeautifulSoup) -> int:
         price = soup.findAll('div', class_='product-card__price')[0].text
         price = price.replace('\n', '').replace(' ', '')
@@ -35,7 +34,6 @@ class Veha(BaseDriver):
             if len(prices) == 1:
                 return int(prices[0])
             return 0
-
 
     def get_product(self, soup):
         h1 = soup.findAll('h1', class_="title-lg", limit=1)
@@ -57,10 +55,35 @@ class Veha(BaseDriver):
         product = Product(name, brand, article, parameters)
         return product
 
-
     def parse_offers(self, soup: BeautifulSoup, link: Link) -> List[Offer]:
         """Возвращает список оферов"""
         product = self.get_product(soup)
         price = self.get_price(soup)
+        shops = self.get_shops(soup)
         # offer = Offer(product=product, price=0, count=0)
         return list()
+
+    def get_shops(self, soup: BeautifulSoup) -> List[Shop]:
+        # https://veha-corp.ru/shop_change/9/
+        # ?change_city=15
+        cities = soup.findAll('div', class_='shops-cities-popup__city-btn')
+        city_dict = dict()
+        for city in cities:
+            key = city.parent.attrs.get('data-tabid')
+            value = city.text
+            city_dict.update({key: value})
+
+        addresses = soup.findAll('li', class_='shops-cities-popup__shop')
+        result = list()
+        for address in addresses:
+            adr = address.text.replace('\n', '')
+            shop_link = address.a.attrs.get('href')
+            shop_link.replace('/shop_change/', '').replace('/', '')
+            ids = re.findall(r'\d+', shop_link)
+            tabid = address.parent.parent.attrs.get('data-tabid')
+            if len(ids) == 1:
+                city = city_dict[tabid]
+                name = f'{city} - {adr}'
+                shop = Shop(name=name, city=city, address=address, shop_param=f'?change_city={ids[0]}')
+                result.append(shop)
+        return result
