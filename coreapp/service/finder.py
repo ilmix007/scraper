43abba870946926e20d162e2a93abf7814c2447f
mendibaev_i <mendibaev_i@ufanet.ru>
2022-06-24 20:55:06 +0500

    update ProuctFace

--- coreapp/service/finder.py
+++ coreapp/service/finder.py
@@ -1,6 +1,10 @@
+from typing import List
+
 from django.db.models import QuerySet
 from coreapp.models import Product
 import logging
+
+from mqttapp.models import ProductResult
 
 LOGGER = logging.getLogger(__name__)
 
@@ -9,6 +13,8 @@ class ProuctFace:
     """Фасад для поиска товаров и предложений"""
 
     @staticmethod
-    def get_products(self, request: str) -> QuerySet:
-        products = Product.objects.filter(article__art__icontains=request)
-        return products
+    def get_products(self, request: str) -> List[ProductResult]:
+        result = list()
+        for prod in Product.objects.filter(article__art__icontains=request):
+            result.append(ProductResult(name=prod.name, art=prod.art, brand=prod.brand))
+        return result
