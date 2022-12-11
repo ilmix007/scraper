5cbf4379a812e8976f384d68249ea754b63d1f96
ilmix <mendibaev_i@ufanet.ru>
2022-12-11 19:18:04 +0500

    updated OfferFace.get_offer_by_art()

--- coreapp/service/offers.py
+++ coreapp/service/offers.py
@@ -1,4 +1,8 @@
 from typing import Dict, List
+from dataclasses import dataclass
+import uuid as uuid
+from django.db.models import F, Value
+from django.db.models.functions import Concat
 
 from coreapp.models import Offer
 
@@ -6,8 +10,21 @@ from coreapp.models import Offer
 class OfferFace:
     @staticmethod
     def get_offer_by_art(article, brand=None) -> List[Dict]:
-        payload = dict(product__article__art=article)
+        filter_payload = dict(product__article__art=article)
         if brand:
-            payload['product__brand__name'] = brand
-        result = Offer.objects.filter(**payload).values('name', 'count', 'price', 'link__url')
-        return result
+            filter_payload['product__brand__name'] = brand
+        annotate_payload = dict(article=F('product__article__art'),
+                                brand=F('product__brand__name'),
+                                shop_name=F('shop__name'),
+                                address=Concat('shop__city__name', Value(' '), 'shop__address'),
+                                product_name=F('name'),
+                                quantity=F('count'),
+                                phone=F('shop__phone'))
+        result = Offer.objects.filter(**filter_payload).annotate(**annotate_payload).values('name', 'article',
+                                                                                            'brand', 'shop_name',
+                                                                                            'address', 'phone',
+                                                                                            'count', 'price',
+                                                                                            'link__url')
+        for item in result:
+            item['link'] = item.pop('link__url')
+        return list(result)
